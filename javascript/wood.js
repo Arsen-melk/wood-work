7ac574aa318352da39b5765f0cdf5dfe07c6ffb5
Arsen-melk <arsenmelk001@gmail.com>
2023-11-13 19:05:55 +0400

    about woodwork

---
+++ javascript/wood.js
@@ -0,0 +1,13 @@
+ 
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 3,
+      spaceBetween: 30,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
+
+
